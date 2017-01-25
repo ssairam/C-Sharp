148c07d7bdd09bad18f146b7fe82d06df2f7a29c
ssairam <ssairam@gmail.com>
2017-01-25 07:20:22 -0500

    Initial Checkin.

---
+++ DataStructuresTests/BinarySearchTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tests
+{
+    [TestClass()]
+    public class BinarySearchTests
+    {
+        private BinarySearch b = new BinarySearch();
+
+        [TestMethod()]
+        public void PrintTreeTest()
+        {
+            b.PrintTree();   
+        }
+
+        [TestMethod()]
+        public void AddToBinaryTreeTest()
+        {
+            b.PrintTree();
+            AddFirstValueToBinaryTree(4);
+            PrintTreeTest();
+            AddLessThanRootValues();
+            PrintTreeTest();
+        }
+
+        private void AddLessThanRootValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddFirstValueToBinaryTree(int v)
+        {
+            b.AddToBinaryTree(4);
+        }
+    }
+}
---
+++ DataStructuresTests/CustomLinkedListTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tests
+{
+    [TestClass()]
+    public class CustomLinkedListTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            CustomLinkedList<int> cll = new CustomLinkedList<int>();
+            cll.Add(1);
+            cll.Add(2);
+            cll.Add(3);
+            cll.PrintList();
+
+        }
+    }
+}
