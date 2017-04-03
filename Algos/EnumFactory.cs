27592bb478c58ef22a228a710b0acc0b5fd2b381
ssairam <ssairam@gmail.com>
2017-04-03 11:39:59 -0400

    Algos

---
+++ Algos/EnumFactory.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algos
+{
+
+    public class EnumFactory
+    {
+        public enum SortTypes{QUICK,MERGE,SELECTION,INSERTION,BUBBLE, BINARY };
+    }
+}
---
+++ Algos/ISort.cs
@@ -0,0 +1,7 @@
+﻿namespace Algos
+{
+    public interface ISort
+    {
+        void Sort(int[] numbers, EnumFactory.SortTypes e);
+    }
+}
